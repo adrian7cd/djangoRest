693303733b043fc3700ac9d3683553f5b011f744
adrian7cd <adrianipsys@proton.me>
2022-09-25 17:38:38 +0200

    Mixins, Html layout

--- backend/django_rest/products/views.py
+++ backend/django_rest/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
@@ -6,6 +6,12 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 
+"""
+#############################################################################################################
+"""
+
+
+# CLASS BASED VIEWS
 class ProductDetailAPIView(generics.RetrieveAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
@@ -51,6 +57,32 @@ class ProductListAPIView(generics.ListAPIView):
   # lookup_field = "pk"
 """
 
+"""
+#############################################################################################################
+"""
+
+# MIXIN GENERIC VIEW
+class ProductMixin(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, generics.GenericAPIView):
+
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_field = "pk"
+
+  def get(self, request, *args, **kwargs):
+    pk = kwargs.get("pk")
+    if pk is not None:
+      return self.retrieve(request, *args, **kwargs)
+    return self.list(request, *args, **kwargs)
+
+  def post(self, request, *args, **kwargs):
+    return self.create(request, *args, **kwargs)
+
+
+"""
+#############################################################################################################
+"""
+
+# FUNCTION BASED VIEW
 @api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
   method = request.method 
